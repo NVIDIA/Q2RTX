a3b735cc386f4de6bcbb46724363aa40e094e456
Andrey Nazarov <skuller@skuller.net>
2011-02-03 16:44:00 +0300

    Remove duplicate d_8to24table definition.

--- src/sw_main.c
+++ src/sw_main.c
@@ -23,8 +23,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 viddef_t    vid;
 
-unsigned    d_8to24table[256];
-
 entity_t    r_worldentity;
 
 refdef_t    r_newrefdef;
