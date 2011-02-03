330cbe5ea9921e2d653d7c6782a5a29e1f4ac263
Andrey Nazarov <skuller@skuller.net>
2011-02-03 16:14:43 +0300

    Fix software renderer crash on 64-bit systems.

--- src/sw_poly.c
+++ src/sw_poly.c
@@ -993,7 +993,7 @@ void R_ClipAndDrawPoly ( float alpha, int isturbulent, qboolean textured )
         scale = yscale * pout->zi;
         pout->v = (ycenter - scale * transformed[1]);
 
-        pv += sizeof (vec5_t) / sizeof (pv);
+        pv += sizeof (vec5_t) / sizeof (vec_t);
     }
 
 // draw it
