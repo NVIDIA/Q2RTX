8d33c4e9cbe8a7ed3d70947789b064f75a83da8d
Andrey Nazarov <skuller@skuller.net>
2011-02-27 17:54:29 +0300

    Don't spam about residual with developer 1.

--- src/sv_main.c
+++ src/sv_main.c
@@ -1678,7 +1678,7 @@ unsigned SV_Frame( unsigned msec ) {
 
     // don't accumulate bogus residual
     if( sv.frameresidual > 250 ) {
-        Com_DPrintf( "Reset residual %u\n", sv.frameresidual );
+        Com_DDPrintf( "Reset residual %u\n", sv.frameresidual );
         sv.frameresidual = 100;
     }
 
