NVIDIA/Q2RTX

commit ae56d347700899841c851e651b00bea0dc4427ed

Fix possible server memory leak.
Rework server shutdown procedure to make it safe to be run even if
server is not fully initalized yet. This fixes a possible memory leak
when ERR_DROP is thrown on initialization (due to missing game library,
for example).

src/mvd_client.c
@@ -381,7 +381,7 @@ int MVD_Frame( void ) {
     if( sv.state == ss_broadcast ) {
         unsigned delta = mvd_suspend_time->value * 60 * 1000;
 
-        if( !delta || !LIST_EMPTY( &svs.client_list ) ) {
+        if( !delta || !LIST_EMPTY( &sv_clientlist ) ) {
             prevtime = svs.realtime;
             if( !mvd_active ) {
                 Com_DPrintf( "Resuming MVD streams.\n" );

src/sv_ccmds.c
@@ -668,7 +668,7 @@ static void SV_Status_f( void ) {
         Com_Printf( "Current map: %s\n\n", sv.name );
     }
 
-    if( LIST_EMPTY( &svs.client_list ) ) {
+    if( LIST_EMPTY( &sv_clientlist ) ) {
         Com_Printf( "No UDP clients.\n" );
     } else {
         if( Cmd_Argc() > 1 ) {
@@ -901,7 +901,6 @@ static void SV_PickClient_f( void ) {
 SV_KillServer_f
 
 Kick everyone off, possibly in preparation for a new game
-
 ===============
 */
 static void SV_KillServer_f( void ) {

src/sv_init.c
@@ -273,6 +273,7 @@ void SV_InitGame( qboolean ismvd ) {
 #endif
 
         CM_FreeMap( &sv.cm );
+        SV_FreeFile( sv.entitystring );
         memset( &sv, 0, sizeof( sv ) );
     }
 
@@ -359,8 +360,6 @@ void SV_InitGame( qboolean ismvd ) {
 
     // send heartbeat very soon
     svs.last_heartbeat = -(HEARTBEAT_SECONDS-5)*1000;
-
-    List_Init( &svs.client_list );
 
     for( i = 0; i < sv_maxclients->integer; i++ ) {
         client = svs.client_pool + i;

src/sv_local.h
@@ -177,7 +177,7 @@ typedef struct {
 #define RATE_MESSAGES   10
 
 #define FOR_EACH_CLIENT( client ) \
-    LIST_FOR_EACH( client_t, client, &svs.client_list, entry )
+    LIST_FOR_EACH( client_t, client, &sv_clientlist, entry )
 
 #define PL_S2C(cl) (cl->frames_sent ? \
     (1.0f-(float)cl->frames_acked/cl->frames_sent)*100.0f : 0.0f)
@@ -372,7 +372,6 @@ typedef struct server_static_s {
     unsigned    realtime;           // always increasing, no clamping, etc
 
     client_t    *client_pool;   // [maxclients]
-    list_t      client_list;    // linked list of non-free clients
 
     unsigned        num_entities;   // maxclients*UPDATE_BACKUP*MAX_PACKET_ENTITIES
     unsigned        next_entity;    // next state to use
@@ -399,6 +398,7 @@ extern list_t      sv_blacklist;
 extern list_t      sv_cmdlist_connect;
 extern list_t      sv_cmdlist_begin;
 extern list_t      sv_filterlist;
+extern list_t      sv_clientlist; // linked list of non-free clients
 
 extern server_static_t     svs;        // persistant server info
 extern server_t            sv;         // local server

src/sv_main.c
@@ -28,6 +28,7 @@ LIST_DECL( sv_blacklist );
 LIST_DECL( sv_cmdlist_connect );
 LIST_DECL( sv_cmdlist_begin );
 LIST_DECL( sv_filterlist );
+LIST_DECL( sv_clientlist ); // linked list of non-free clients
 
 client_t    *sv_client;         // current client
 
@@ -106,7 +107,8 @@ void SV_RemoveClient( client_t *client ) {
         client->netchan = NULL;
     }
 
-    // unlink them from active client list
+    // unlink them from active client list, but don't clear the list entry
+    // itself to make code that traverses client list in a loop happy!
     List_Remove( &client->entry );
 
 #if USE_MVD_CLIENT
@@ -818,10 +820,8 @@ static void SVC_DirectConnect( void ) {
             }
 
             Com_DPrintf( "%s: reconnect\n", NET_AdrToString( &net_from ) );
-            newcl = cl;
-
-            // NOTE: it's safe to call SV_Remove since we exit the loop
             SV_RemoveClient( cl );
+            newcl = cl;
             break;
         }
     }
@@ -980,7 +980,7 @@ static void SVC_DirectConnect( void ) {
     }
 
     // add them to the linked list of connected clients
-    List_SeqAdd( &svs.client_list, &newcl->entry );
+    List_SeqAdd( &sv_clientlist, &newcl->entry );
 
     Com_DPrintf( "Going from cs_free to cs_assigned for %s\n", newcl->name );
     newcl->state = cs_assigned;
@@ -1452,7 +1452,7 @@ static inline qboolean check_paused( void ) {
 #if USE_MVD_CLIENT
         LIST_EMPTY( &mvd_gtv_list ) &&
 #endif
-        LIST_SINGLE( &svs.client_list ) )
+        LIST_SINGLE( &sv_clientlist ) )
     {
         if( !sv_paused->integer ) {
             Cvar_Set( "sv_paused", "1" );
@@ -1576,6 +1576,11 @@ Informs all masters that this server is going down
 */
 static void SV_MasterShutdown( void ) {
     master_t *m;
+
+    // reset ack times
+    FOR_EACH_MASTER( m ) {
+        m->last_ack = 0;
+    }
 
     if( !Com_IsDedicated() )
         return;        // only dedicated servers send heartbeats
@@ -1981,12 +1986,17 @@ Used by SV_Shutdown to send a final message to all
 connected clients before the server goes down. The messages are sent
 immediately, not just stuck on the outgoing message list, because the
 server is going to totally exit after returning from this function.
+
+Also resposible for freeing all clients.
 ==================
 */
-static void SV_FinalMessage( const char *message, int cmd ) {
+static void SV_FinalMessage( const char *message, error_type_t type ) {
     client_t    *client;
     netchan_t   *netchan;
     int         i;
+
+    if( LIST_EMPTY( &sv_clientlist ) )
+        return;
 
     if( message ) {
         MSG_WriteByte( svc_print );
@@ -1994,7 +2004,10 @@ static void SV_FinalMessage( const char *message, int cmd ) {
         MSG_WriteString( message );
     }
 
-    MSG_WriteByte( cmd );
+    if( type == ERR_RECONNECT )
+        MSG_WriteByte( svc_reconnect );
+    else
+        MSG_WriteByte( svc_disconnect );
 
     // send it twice
     // stagger the packets to crutch operating system limited buffers
@@ -2020,6 +2033,8 @@ static void SV_FinalMessage( const char *message, int cmd ) {
         }
         SV_RemoveClient( client );
     }
+
+    List_Init( &sv_clientlist );
 }
 
 
@@ -2027,37 +2042,28 @@ static void SV_FinalMessage( const char *message, int cmd ) {
 ================
 SV_Shutdown
 
-Called when each game quits, from Com_Quit or Com_Error
+Called when each game quits, from Com_Quit or Com_Error.
+Should be safe to call even if server is not fully initalized yet.
 ================
 */
 void SV_Shutdown( const char *finalmsg, error_type_t type ) {
-    master_t *m;
-
-    Cvar_Set( "sv_running", "0" );
-    Cvar_Set( "sv_paused", "0" );
-
-    if( !svs.initialized ) {
 #if USE_MVD_CLIENT
-        MVD_Shutdown(); // make sure MVD client is down
-#endif
-        return;
+    if( ge != &mvd_ge ) {
+        // shutdown MVD client now if not running the built-in MVD game module,
+        // otherwise SV_ShutdownGameProgs will take care of this
+        MVD_Shutdown();
     }
+#endif
 
 #if USE_AC_SERVER
     AC_Disconnect();
 #endif
 
 #if USE_MVD_SERVER
-    // shutdown MVD server
     SV_MvdShutdown( type );
 #endif
 
-    if( type == ERR_RECONNECT ) {
-        SV_FinalMessage( finalmsg, svc_reconnect );
-    } else {
-        SV_FinalMessage( finalmsg, svc_disconnect );
-    }
-
+    SV_FinalMessage( finalmsg, type );
     SV_MasterShutdown();
     SV_ShutdownGameProgs();
 
@@ -2074,16 +2080,14 @@ void SV_Shutdown( const char *finalmsg, error_type_t type ) {
 #endif
     memset( &svs, 0, sizeof( svs ) );
 
-    // reset masters
-    FOR_EACH_MASTER( m ) {
-        m->last_ack = 0;
-    }
-
     // reset rate limits
     init_rate_limits();
 
     sv_client = NULL;
     sv_player = NULL;
+
+    Cvar_Set( "sv_running", "0" );
+    Cvar_Set( "sv_paused", "0" );
 
 #if USE_SYSCON
     SV_SetConsoleTitle();

commit 7ab786489fffa077ba0020cfea587f8e077c1c59

Fix MVD client crash at server restart.
Commit ae56d347 introduced a bug that caused MVD client to crash when
started from an initialized game server state. Fix it by passing an
internal flag to SV_Shutdown.

src/mvd_client.c
@@ -1071,7 +1071,9 @@ static void parse_hello( gtv_t *gtv ) {
     Com_Printf( "[%s] -=- Server hello done.\n", gtv->name );
 
     if( sv.state != ss_broadcast ) {
-        MVD_Spawn_f(); // the game is just starting
+        // the game is just starting
+        SV_InitGame( MVD_SPAWN_INTERNAL );
+        MVD_Spawn();
     } else {
         // notify spectators
         if( Com_IsDedicated() && gtv->mvd ) {
@@ -1592,9 +1594,7 @@ OPERATOR COMMANDS
 ====================================================================
 */
 
-void MVD_Spawn_f( void ) {
-    SV_InitGame( qtrue );
-
+void MVD_Spawn( void ) {
     Cvar_SetInteger( sv_running, ss_broadcast, FROM_CODE );
     Cvar_Set( "sv_paused", "0" );
     Cvar_Set( "timedemo", "0" );
@@ -1609,6 +1609,11 @@ void MVD_Spawn_f( void ) {
     sv.spawncount &= 0x7FFFFFFF;
 
     sv.state = ss_broadcast;
+}
+
+static void MVD_Spawn_f( void ) {
+    SV_InitGame( MVD_SPAWN_ENABLED );
+    MVD_Spawn();
 }
 
 static void list_generic( void ) {

src/mvd_local.h
@@ -193,7 +193,7 @@ mvd_t *MVD_SetChannel( int arg );
 
 void MVD_File_g( genctx_t *ctx );
 
-void MVD_Spawn_f( void ); 
+void MVD_Spawn( void );
 
 void MVD_StopRecord( mvd_t *mvd );
 

src/mvd_parse.c
@@ -881,7 +881,9 @@ static void MVD_ChangeLevel( mvd_t *mvd ) {
     mvd_client_t *client;
 
     if( sv.state != ss_broadcast ) {
-        MVD_Spawn_f(); // the game is just starting
+        // the game is just starting
+        SV_InitGame( MVD_SPAWN_INTERNAL );
+        MVD_Spawn();
         return;
     }
 

src/sv_ac.c
@@ -1404,7 +1404,7 @@ void AC_Run( void ) {
     }
 }
 
-void AC_Connect( qboolean ismvd ) {
+void AC_Connect( unsigned mvd_spawn ) {
     int attempts;
 
     if( !ac_required->integer ) {
@@ -1418,7 +1418,7 @@ void AC_Connect( qboolean ismvd ) {
         return;
     }
 #endif
-    if( ismvd ) {
+    if( mvd_spawn ) {
         Com_Printf( "ANTICHEAT: Only supported on game servers, disabling.\n" );
         Cvar_SetByVar( ac_required, "0", FROM_CODE );
         return;

src/sv_ccmds.c
@@ -306,7 +306,7 @@ static void SV_Map( int argnum, qboolean restart ) {
 
     // any error will drop from this point
     if( sv.state != ss_game || restart ) {
-        SV_InitGame( qfalse );  // the game is just starting
+        SV_InitGame( MVD_SPAWN_DISABLED );  // the game is just starting
     }
 
     SV_SpawnServer( &cm, s, spawnpoint );

src/sv_init.c
@@ -254,17 +254,17 @@ void SV_SpawnServer( cm_t *cm, const char *server, const char *spawnpoint ) {
 SV_InitGame
 
 A brand new game has been started.
-If ismvd is true, load the built-in MVD game module.
+If mvd_spawn is non-zero, load the built-in MVD game module.
 ==============
 */
-void SV_InitGame( qboolean ismvd ) {
+void SV_InitGame( unsigned mvd_spawn ) {
     int     i, entnum;
     edict_t *ent;
     client_t *client;
 
     if( svs.initialized ) {
         // cause any connected clients to reconnect
-        SV_Shutdown( "Server restarted\n", ERR_RECONNECT );
+        SV_Shutdown( "Server restarted\n", ERR_RECONNECT | mvd_spawn );
     } else {
 #if USE_CLIENT
         // make sure the client is down
@@ -284,7 +284,7 @@ void SV_InitGame( qboolean ismvd ) {
     Cvar_Reset( sv_recycle );
 #endif
 
-    if( ismvd ) {
+    if( mvd_spawn ) {
         Cvar_Set( "deathmatch", "1" );
         Cvar_Set( "coop", "0" );
     } else {
@@ -329,7 +329,7 @@ void SV_InitGame( qboolean ismvd ) {
 
 #if USE_MVD_SERVER
     // initialize MVD server
-    if( !ismvd ) {
+    if( !mvd_spawn ) {
         SV_MvdInit();
     }
 #endif
@@ -348,7 +348,7 @@ void SV_InitGame( qboolean ismvd ) {
 
     // init game
 #if USE_MVD_CLIENT
-    if( ismvd ) {
+    if( mvd_spawn ) {
         if( ge ) {
             SV_ShutdownGameProgs();
         }
@@ -371,7 +371,7 @@ void SV_InitGame( qboolean ismvd ) {
     }
 
 #if USE_AC_SERVER
-    AC_Connect( ismvd );
+    AC_Connect( mvd_spawn );
 #endif
 
     svs.initialized = qtrue;

src/sv_local.h
@@ -73,6 +73,12 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define SV_FEATURES (GMF_CLIENTNUM|GMF_PROPERINUSE|GMF_MVDSPEC|\
                      GMF_WANT_ALL_DISCONNECTS|GMF_ENHANCED_SAVEGAMES)
 
+// ugly hack for SV_Shutdown
+#define MVD_SPAWN_DISABLED  0
+#define MVD_SPAWN_ENABLED   0x40000000
+#define MVD_SPAWN_INTERNAL  0x80000000
+#define MVD_SPAWN_MASK      0xc0000000
+
 typedef struct {
     unsigned    num_entities;
     unsigned    first_entity;
@@ -492,7 +498,7 @@ void Master_Packet (void);
 //
 void SV_ClientReset( client_t *client );
 void SV_SpawnServer( cm_t *cm, const char *server, const char *spawnpoint );
-void SV_InitGame( qboolean ismvd );
+void SV_InitGame( unsigned mvd_spawn );
 
 //
 // sv_send.c
@@ -559,7 +565,7 @@ void AC_ClientToken( client_t *cl, const char *token );
 
 void AC_Register( void );
 void AC_Disconnect( void );
-void AC_Connect( qboolean ismvd );
+void AC_Connect( unsigned mvd_spawn );
 void AC_Run( void );
 
 void AC_List_f( void );

src/sv_main.c
@@ -2038,7 +2038,6 @@ static void SV_FinalMessage( const char *message, error_type_t type ) {
     List_Init( &sv_clientlist );
 }
 
-
 /*
 ================
 SV_Shutdown
@@ -2049,11 +2048,12 @@ Should be safe to call even if server is not fully initalized yet.
 */
 void SV_Shutdown( const char *finalmsg, error_type_t type ) {
 #if USE_MVD_CLIENT
-    if( ge != &mvd_ge ) {
-        // shutdown MVD client now if not running the built-in MVD game module,
-        // otherwise SV_ShutdownGameProgs will take care of this
+    if( ge != &mvd_ge && !(type & MVD_SPAWN_INTERNAL) ) {
+        // shutdown MVD client now if not already running the built-in MVD game module
+        // don't shutdown if called from internal MVD spawn function (ugly hack)!
         MVD_Shutdown();
     }
+    type &= ~MVD_SPAWN_MASK;
 #endif
 
 #if USE_AC_SERVER

src/sv_save.c
@@ -313,7 +313,7 @@ static qerror_t read_server_file( const char *dir ) {
     }
 
     // start a new game fresh with new cvars
-    SV_InitGame( qfalse );
+    SV_InitGame( MVD_SPAWN_DISABLED );
 
     // error out immediately if game doesn't support safe savegames
     if( !( g_features->integer & GMF_ENHANCED_SAVEGAMES ) ) {