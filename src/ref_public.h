d5fbf41127854046367bcb590c361986c3a110be
Andrey Nazarov <skuller@skuller.net>
2012-03-24 15:42:01 +0400

    Double MAX_ENTITIES constant.
    Provide more space for local and linked entities by making MAX_ENTITIES
    twice the size of MAX_PACKET_ENTITIES.

--- src/ref_public.h
+++ src/ref_public.h
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define __REF_H
 
 #define MAX_DLIGHTS     32
-#define MAX_ENTITIES    128
+#define MAX_ENTITIES    256     // == MAX_PACKET_ENTITIES * 2
 #define MAX_PARTICLES   4096
 #define MAX_LIGHTSTYLES 256
 
