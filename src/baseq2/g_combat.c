6bf61a3340339340689fad1193cfd75b4237c816
kugelrund <marcel.jacobse@ewetel.net>
2017-02-26 16:53:44 +0100

    Fix nofriendlyfire being applied to AI in coop

--- src/baseq2/g_combat.c
+++ src/baseq2/g_combat.c
@@ -382,7 +382,7 @@ void T_Damage(edict_t *targ, edict_t *inflictor, edict_t *attacker, vec3_t dir, 
     // friendly fire avoidance
     // if enabled you can't hurt teammates (but you can hurt yourself)
     // knockback still occurs
-    if ((targ != attacker) && ((deathmatch->value && ((int)(dmflags->value) & (DF_MODELTEAMS | DF_SKINTEAMS))) || coop->value)) {
+    if ((targ != attacker) && ((deathmatch->value && ((int)(dmflags->value) & (DF_MODELTEAMS | DF_SKINTEAMS))) || (coop->value && targ->client))) {
         if (OnSameTeam(targ, attacker)) {
             if ((int)(dmflags->value) & DF_NO_FRIENDLY_FIRE)
                 damage = 0;
