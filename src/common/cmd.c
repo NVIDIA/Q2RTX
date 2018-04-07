148a311b0051abdb511d4949a28a8d3c409dc34f
Andrey Nazarov <skuller@skuller.net>
2018-04-08 01:35:03 +0300

    Re-indent declarations.

--- src/common/cmd.c
+++ src/common/cmd.c
@@ -802,25 +802,25 @@ typedef struct cmd_function_s {
     char            *name;
 } cmd_function_t;
 
-static  list_t  cmd_functions;        // possible commands to execute
-static  list_t  cmd_hash[CMD_HASH_SIZE];
+static list_t   cmd_functions;      // possible commands to execute
+static list_t   cmd_hash[CMD_HASH_SIZE];
 
-static  int     cmd_argc;
-static  char    *cmd_argv[MAX_STRING_TOKENS]; // pointers to cmd_data[]
-static  char    *cmd_null_string = "";
+static int      cmd_argc;
+static char     *cmd_argv[MAX_STRING_TOKENS]; // pointers to cmd_data[]
+static char     *cmd_null_string = "";
 
 // complete command string, left untouched
-static  char    cmd_string[MAX_STRING_CHARS];
-static  int     cmd_string_len;
+static char     cmd_string[MAX_STRING_CHARS];
+static int      cmd_string_len;
 
 // offsets of individual tokens into cmd_string
-static  int     cmd_offsets[MAX_STRING_TOKENS];
+static int      cmd_offsets[MAX_STRING_TOKENS];
 
 // sequence of NULL-terminated, normalized tokens
-static  char    cmd_data[MAX_STRING_CHARS];
+static char     cmd_data[MAX_STRING_CHARS];
 
 // normalized command arguments
-static  char    cmd_args[MAX_STRING_CHARS];
+static char     cmd_args[MAX_STRING_CHARS];
 
 int             cmd_optind;
 char            *cmd_optarg;
