14f2dd5bb9c3f276ec1c10c70e21dbc220121de1
Andrey Nazarov <skuller@skuller.net>
2018-04-26 20:04:18 +0300

    Clamp ‘s_mixahead’ cvar to sane range.

--- src/client/sound/dma.c
+++ src/client/sound/dma.c
@@ -167,7 +167,7 @@ void DMA_Update(void)
     }
 
 // mix ahead of current position
-    endtime = soundtime + s_mixahead->value * dma.speed;
+    endtime = soundtime + Cvar_ClampValue(s_mixahead, 0, 1) * dma.speed;
 
     // mix to an even submission block size
     endtime = ALIGN(endtime, dma.submission_chunk);
