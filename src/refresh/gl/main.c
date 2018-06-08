085d69c77beed6b768f27f49b8bc1d8ef2522547
Andrey Nazarov <skuller@skuller.net>
2018-06-08 18:16:04 +0300

    Don't crash if GL_SetupConfig() generates GL errors.

--- src/refresh/gl/main.c
+++ src/refresh/gl/main.c
@@ -761,7 +761,7 @@ static void GL_Unregister(void)
     Cmd_RemoveCommand("strings");
 }
 
-static qboolean GL_SetupConfig(void)
+static void GL_SetupConfig(void)
 {
     GLint integer;
 
@@ -778,9 +778,6 @@ static qboolean GL_SetupConfig(void)
 
     qglGetIntegerv(GL_STENCIL_BITS, &integer);
     gl_config.stencilbits = integer;
-
-    GL_ShowErrors(__func__);
-    return qtrue;
 }
 
 static void GL_InitTables(void)
@@ -841,10 +838,8 @@ qboolean R_Init_GL(qboolean total)
         goto fail;
     }
 
-    // initialize extensions and get various limits from OpenGL
-    if (!GL_SetupConfig()) {
-        goto fail;
-    }
+    // get various limits from OpenGL
+    GL_SetupConfig();
 
     // register our variables
     GL_Register();
