NVIDIA/Q2RTX

commit 5406a1bf6c608154055a7a429aacacfe72eff3e1

Added support for the final official KHR ray tracing extensions (v11). Squashed from a feature branch. Both NV_ray_tracing and KHR_ray_tracing_pipeline etc. are supported. When both are available, the KHR extensions are preferred, unless cvar 'nv_ray_tracing' is nonzero. Includes work done by Joshua Ashton in https://github.com/NVIDIA/Q2RTX/pull/73

src/refresh/vkpt/textures.c
@@ -646,7 +646,7 @@ IMG_Unload_RTX(image_t *image)
 
 	if (tex_images[index])
 	{
-	const uint32_t frame_index = (qvk.frame_counter + MAX_FRAMES_IN_FLIGHT) % DESTROY_LATENCY;
+	const uint32_t frame_index = (qvk.frame_counter + MAX_FRAMES_IN_FLIGHT + 1) % DESTROY_LATENCY;
 	UnusedResources* unused_resources = texture_system.unused_resources + frame_index;
 
 	const uint32_t unused_index = unused_resources->image_num++;
@@ -1077,9 +1077,9 @@ vkpt_textures_destroy()
 }
 
 #ifdef VKPT_DEVICE_GROUPS
-static VkMemoryAllocateFlagsInfoKHR mem_alloc_flags_broadcast = {
-		.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_FLAGS_INFO_KHR,
-		.flags = VK_MEMORY_ALLOCATE_DEVICE_MASK_BIT_KHR,
+static VkMemoryAllocateFlagsInfo mem_alloc_flags_broadcast = {
+		.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_FLAGS_INFO,
+		.flags = VK_MEMORY_ALLOCATE_DEVICE_MASK_BIT,
 };
 #endif
 
@@ -1521,8 +1521,8 @@ LIST_IMAGES_A_B
 					vkGetImageMemoryRequirements(qvk.device, qvk.images_local[d][i], &mem_req);
 				}
 
-				VkBindImageMemoryDeviceGroupInfoKHR device_group_info = {
-					.sType = VK_STRUCTURE_TYPE_BIND_IMAGE_MEMORY_DEVICE_GROUP_INFO_KHR,
+				VkBindImageMemoryDeviceGroupInfo device_group_info = {
+					.sType = VK_STRUCTURE_TYPE_BIND_IMAGE_MEMORY_DEVICE_GROUP_INFO,
 					.pNext = NULL,
 					.deviceIndexCount = qvk.device_count,
 					.pDeviceIndices = device_indices,
@@ -1530,15 +1530,15 @@ LIST_IMAGES_A_B
 					.pSplitInstanceBindRegions = NULL,
 				};
 
-				VkBindImageMemoryInfoKHR bind_info = {
-					.sType = VK_STRUCTURE_TYPE_BIND_IMAGE_MEMORY_INFO_KHR,
+				VkBindImageMemoryInfo bind_info = {
+					.sType = VK_STRUCTURE_TYPE_BIND_IMAGE_MEMORY_INFO,
 					.pNext = &device_group_info,
 					.image = qvk.images_local[d][i],
 					.memory = mem_images[i],
 					.memoryOffset = 0,
 				};
 
-				_VK(qvkBindImageMemory2KHR(qvk.device, 1, &bind_info));
+				_VK(vkBindImageMemory2(qvk.device, 1, &bind_info));
 			}
 		}
 #endif
@@ -1626,7 +1626,7 @@ LIST_IMAGES_A_B
 		[VKPT_IMG_##_name] = { \
 			.imageLayout = VK_IMAGE_LAYOUT_GENERAL, \
 			.imageView   = qvk.images_views[VKPT_IMG_##_name], \
-			.sampler     = qvk.tex_sampler, \
+			.sampler     = qvk.tex_sampler_nearest, \
 		},
 
 		LIST_IMAGES
@@ -1637,6 +1637,8 @@ LIST_IMAGES_A_B
 	for(int i = VKPT_IMG_BLOOM_HBLUR; i <= VKPT_IMG_BLOOM_VBLUR; i++) {
 		img_info[i].sampler = qvk.tex_sampler_linear_clamp;
 	}
+	img_info[VKPT_IMG_ASVGF_TAA_A].sampler = qvk.tex_sampler;
+	img_info[VKPT_IMG_ASVGF_TAA_B].sampler = qvk.tex_sampler;
 
 	VkWriteDescriptorSet output_img_write[NUM_IMAGES * 2];
 

cmake/compileShaders.cmake
@@ -29,8 +29,8 @@ message(STATUS "Glslang compiler : ${GLSLANG_COMPILER}")
 
 function(compile_shader)
     set(options "")
-    set(oneValueArgs SOURCE_FILE OUTPUT_FILE_LIST)
-    set(multiValueArgs "")
+    set(oneValueArgs SOURCE_FILE OUTPUT_FILE_NAME OUTPUT_FILE_LIST)
+    set(multiValueArgs DEFINES)
     cmake_parse_arguments(params "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     if (NOT params_SOURCE_FILE)
@@ -42,37 +42,23 @@ function(compile_shader)
     endif()
 
     set(src_file "${CMAKE_CURRENT_SOURCE_DIR}/${params_SOURCE_FILE}")
-    get_filename_component(base_file_name ${src_file} NAME)
-    get_filename_component(file_extension ${src_file} EXT)
 
-    if (file_extension STREQUAL ".comp")
-        set(DEFINES "-DSHADER_STAGE_COMP")
-    elseif(file_extension STREQUAL ".rahit")
-        set(DEFINES "-DSHADER_STAGE_ACHIT")
-    elseif(file_extension STREQUAL ".rmiss")
-        set(DEFINES "-DSHADER_STAGE_RMISS")
-    elseif(file_extension STREQUAL ".rchit")
-        set(DEFINES "-DSHADER_STAGE_RCHIT")
-    elseif(file_extension STREQUAL ".rgen")
-        set(DEFINES "-DSHADER_STAGE_RGEN")
-    elseif(file_extension STREQUAL ".frag")
-        set(DEFINES "-DSHADER_STAGE_FRAG")
-    elseif(file_extension STREQUAL ".vert")
-        set(DEFINES "-DSHADER_STAGE_VERT")
+    if (params_OUTPUT_FILE_NAME)
+        set(output_file_name ${params_OUTPUT_FILE_NAME})
     else()
-        message(FATAL_ERROR "unknown extension in shader source file: ${file_extension}")
+        get_filename_component(output_file_name ${src_file} NAME)
     endif()
-
+    
     set_source_files_properties(${src_file} PROPERTIES VS_TOOL_OVERRIDE "None")
 
     set (out_dir "${CMAKE_SOURCE_DIR}/baseq2/shader_vkpt")
-    set (out_file "${out_dir}/${base_file_name}.spv")
+    set (out_file "${out_dir}/${output_file_name}.spv")
     
     set(glslang_command_line
-            --target-env vulkan1.1
+            --target-env vulkan1.2
             -DVKPT_SHADER
             -V
-            ${DEFINES}
+            ${params_DEFINES}
             "${src_file}"
             -o "${out_file}")
 

src/CMakeLists.txt
@@ -297,17 +297,7 @@ set(SRC_SHADERS
 	refresh/vkpt/shader/bloom_composite.comp
 	refresh/vkpt/shader/bloom_blur.comp
 	refresh/vkpt/shader/compositing.comp
-	refresh/vkpt/shader/primary_rays.rgen
-	refresh/vkpt/shader/direct_lighting.rgen
 	refresh/vkpt/shader/checkerboard_interleave.comp
-	refresh/vkpt/shader/indirect_lighting.rgen
-	refresh/vkpt/shader/path_tracer.rchit
-	refresh/vkpt/shader/path_tracer.rmiss
-	refresh/vkpt/shader/path_tracer_particle.rahit
-	refresh/vkpt/shader/path_tracer_sprite.rahit
-	refresh/vkpt/shader/path_tracer_beam.rahit
-	refresh/vkpt/shader/path_tracer_explosion.rahit
-	refresh/vkpt/shader/path_tracer_shadow.rmiss
 	refresh/vkpt/shader/asvgf_atrous.comp
 	refresh/vkpt/shader/asvgf_gradient_atrous.comp
 	refresh/vkpt/shader/asvgf_gradient_img.comp
@@ -321,7 +311,6 @@ set(SRC_SHADERS
     refresh/vkpt/shader/tone_mapping_apply.comp
 	refresh/vkpt/shader/physical_sky.comp
 	refresh/vkpt/shader/physical_sky_space.comp
-	refresh/vkpt/shader/reflect_refract.rgen
 	refresh/vkpt/shader/shadow_map.vert
 	refresh/vkpt/shader/sky_buffer_resolve.comp
 	refresh/vkpt/shader/stretch_pic.frag
@@ -330,9 +319,29 @@ set(SRC_SHADERS
 	refresh/vkpt/shader/final_blit.vert
 )
 
+set(SRC_RT_SHADERS
+	refresh/vkpt/shader/primary_rays.rgen
+	refresh/vkpt/shader/direct_lighting.rgen
+	refresh/vkpt/shader/indirect_lighting.rgen
+	refresh/vkpt/shader/path_tracer.rchit
+	refresh/vkpt/shader/path_tracer.rmiss
+	refresh/vkpt/shader/path_tracer_particle.rahit
+	refresh/vkpt/shader/path_tracer_sprite.rahit
+	refresh/vkpt/shader/path_tracer_beam.rahit
+	refresh/vkpt/shader/path_tracer_explosion.rahit
+	refresh/vkpt/shader/path_tracer_shadow.rmiss
+	refresh/vkpt/shader/reflect_refract.rgen
+)
+
 include(../cmake/compileShaders.cmake)
 
 if (GLSLANG_COMPILER)
+	foreach(s ${SRC_RT_SHADERS})
+        get_filename_component(shader_name ${s} NAME)
+		compile_shader(SOURCE_FILE ${s} OUTPUT_FILE_LIST shader_bytecode OUTPUT_FILE_NAME "${shader_name}.nv" DEFINES "-DNV_RAY_TRACING")
+		compile_shader(SOURCE_FILE ${s} OUTPUT_FILE_LIST shader_bytecode OUTPUT_FILE_NAME "${shader_name}.khr" DEFINES "-DKHR_RAY_TRACING")
+	endforeach()
+
 	foreach(s ${SRC_SHADERS})
 		compile_shader(SOURCE_FILE ${s} OUTPUT_FILE_LIST shader_bytecode)
 	endforeach()

src/client/refresh.c
@@ -35,6 +35,9 @@ cvar_t      *_vid_fullscreen;
 cvar_t      *vid_display;
 cvar_t      *vid_displaylist;
 
+// used in gl and vkpt renderers
+int registration_sequence;
+
 #define MODE_GEOMETRY   1
 #define MODE_FULLSCREEN 2
 #define MODE_MODELIST   4

src/refresh/gl/main.c
@@ -31,8 +31,6 @@ statCounters_t  c;
 
 entity_t gl_world;
 
-int registration_sequence;
-
 // regular variables
 cvar_t *gl_partscale;
 cvar_t *gl_partstyle;

src/refresh/vkpt/asvgf.c
@@ -136,22 +136,22 @@ vkpt_asvgf_create_pipelines()
 		[ATROUS_ITER_0] = {
 			.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
 			.stage = SHADER_STAGE_SPEC(QVK_MOD_ASVGF_ATROUS_COMP, VK_SHADER_STAGE_COMPUTE_BIT, &specInfo[0]),
-			.layout = pipeline_layout_atrous,
+			.layout = pipeline_layout_general,
 		},
 		[ATROUS_ITER_1] = {
 			.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
 			.stage = SHADER_STAGE_SPEC(QVK_MOD_ASVGF_ATROUS_COMP, VK_SHADER_STAGE_COMPUTE_BIT, &specInfo[1]),
-			.layout = pipeline_layout_atrous,
+			.layout = pipeline_layout_general,
 		},
 		[ATROUS_ITER_2] = {
 			.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
 			.stage = SHADER_STAGE_SPEC(QVK_MOD_ASVGF_ATROUS_COMP, VK_SHADER_STAGE_COMPUTE_BIT, &specInfo[2]),
-			.layout = pipeline_layout_atrous,
+			.layout = pipeline_layout_general,
 		},
 		[ATROUS_ITER_3] = {
 			.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
 			.stage = SHADER_STAGE_SPEC(QVK_MOD_ASVGF_ATROUS_COMP, VK_SHADER_STAGE_COMPUTE_BIT, &specInfo[3]),
-			.layout = pipeline_layout_atrous,
+			.layout = pipeline_layout_general,
 		},
 		[CHECKERBOARD_INTERLEAVE] = {
 			.sType  = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
@@ -247,7 +247,7 @@ vkpt_asvgf_filter(VkCommandBuffer cmd_buf, qboolean enable_lf)
 	/* create gradient image */
 	vkCmdBindPipeline(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_asvgf[GRADIENT_IMAGE]);
 	vkCmdBindDescriptorSets(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE,
-		pipeline_layout_atrous, 0, LENGTH(desc_sets), desc_sets, 0, 0);
+		pipeline_layout_general, 0, LENGTH(desc_sets), desc_sets, 0, 0);
 	vkCmdDispatch(cmd_buf,
 			(qvk.gpu_slice_width / GRAD_DWN + 15) / 16,
 			(qvk.extent_render.height / GRAD_DWN + 15) / 16,
@@ -257,7 +257,7 @@ vkpt_asvgf_filter(VkCommandBuffer cmd_buf, qboolean enable_lf)
 	BARRIER_COMPUTE(cmd_buf, qvk.images[VKPT_IMG_ASVGF_GRAD_LF_PING]);
 	BARRIER_COMPUTE(cmd_buf, qvk.images[VKPT_IMG_ASVGF_GRAD_HF_SPEC_PING]);
 
-	//vkCmdBindPipeline(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_asvgf[GRADIENT_ATROUS]);
+	vkCmdBindPipeline(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_asvgf[GRADIENT_ATROUS]);
 	vkCmdBindDescriptorSets(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE,
 		pipeline_layout_atrous, 0, LENGTH(desc_sets), desc_sets, 0, 0);
 
@@ -270,8 +270,6 @@ vkpt_asvgf_filter(VkCommandBuffer cmd_buf, qboolean enable_lf)
 
 		vkCmdPushConstants(cmd_buf, pipeline_layout_atrous,
 			VK_SHADER_STAGE_COMPUTE_BIT, 0, sizeof(push_constants), push_constants);
-
-		vkCmdBindPipeline(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_asvgf[GRADIENT_ATROUS]);
 
 		vkCmdDispatch(cmd_buf,
 				(qvk.gpu_slice_width / GRAD_DWN + 15) / 16,
@@ -288,7 +286,7 @@ vkpt_asvgf_filter(VkCommandBuffer cmd_buf, qboolean enable_lf)
 	/* temporal accumulation / filtering */
 	vkCmdBindPipeline(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_asvgf[TEMPORAL]);
 	vkCmdBindDescriptorSets(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE,
-		pipeline_layout_atrous, 0, LENGTH(desc_sets), desc_sets, 0, 0);
+		pipeline_layout_general, 0, LENGTH(desc_sets), desc_sets, 0, 0);
 	vkCmdDispatch(cmd_buf,
 			(qvk.gpu_slice_width + 14) / 15,
 			(qvk.extent_render.height + 14) / 15,
@@ -306,10 +304,6 @@ vkpt_asvgf_filter(VkCommandBuffer cmd_buf, qboolean enable_lf)
 	END_PERF_MARKER(cmd_buf, PROFILER_ASVGF_TEMPORAL);
 	BEGIN_PERF_MARKER(cmd_buf, PROFILER_ASVGF_ATROUS);
 
-	//vkCmdBindPipeline(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_asvgf[ATROUS]);
-	vkCmdBindDescriptorSets(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE,
-		pipeline_layout_atrous, 0, LENGTH(desc_sets), desc_sets, 0, 0);
-
 	/* spatial reconstruction filtering */
 	const int num_atrous_iterations = 4;
 	for(int i = 0; i < num_atrous_iterations; i++) 
@@ -320,10 +314,13 @@ vkpt_asvgf_filter(VkCommandBuffer cmd_buf, qboolean enable_lf)
 				i
 			};
 
+			vkCmdBindPipeline(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_asvgf[ATROUS_LF]);
+
+			vkCmdBindDescriptorSets(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE,
+				pipeline_layout_atrous, 0, LENGTH(desc_sets), desc_sets, 0, 0);
+
 			vkCmdPushConstants(cmd_buf, pipeline_layout_atrous,
 				VK_SHADER_STAGE_COMPUTE_BIT, 0, sizeof(push_constants), push_constants);
-
-			vkCmdBindPipeline(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_asvgf[ATROUS_LF]);
 
 			vkCmdDispatch(cmd_buf,
 				(qvk.gpu_slice_width / GRAD_DWN + 15) / 16,
@@ -340,6 +337,10 @@ vkpt_asvgf_filter(VkCommandBuffer cmd_buf, qboolean enable_lf)
 		int specialization = ATROUS_ITER_0 + i;
 
 		vkCmdBindPipeline(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_asvgf[specialization]);
+
+		vkCmdBindDescriptorSets(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE,
+			pipeline_layout_general, 0, LENGTH(desc_sets), desc_sets, 0, 0);
+
 		vkCmdDispatch(cmd_buf,
 				(qvk.gpu_slice_width + 15) / 16,
 				(qvk.extent_render.height + 15) / 16,

src/refresh/vkpt/device_memory_allocator.c
@@ -133,9 +133,9 @@ int create_sub_allocator(DeviceMemoryAllocator* allocator, uint32_t memory_type)
 	};
 
 #ifdef VKPT_DEVICE_GROUPS
-	VkMemoryAllocateFlagsInfoKHR mem_alloc_flags = {
-		.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_FLAGS_INFO_KHR,
-		.flags = VK_MEMORY_ALLOCATE_DEVICE_MASK_BIT_KHR,
+	VkMemoryAllocateFlagsInfo mem_alloc_flags = {
+		.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_FLAGS_INFO,
+		.flags = VK_MEMORY_ALLOCATE_DEVICE_MASK_BIT,
 		.deviceMask = (1 << qvk.device_count) - 1
 	};
 

src/refresh/vkpt/draw.c
@@ -30,7 +30,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #define TEXNUM_WHITE (~0)
 #define MAX_STRETCH_PICS (1<<14)
 
-drawStatic_t draw = {
+static drawStatic_t draw = {
 	.scale = 1.0f,
 	.alpha_scale = 1.0f
 };

commit 243ccbb3bbf6a6e4a73b7ccf137e33aaf45eaae5

Fixed the Lanczos final blit filter that was broken since commit 5406a1bf6c608154055a7a429aacacfe72eff3e1.

src/refresh/vkpt/textures.c
@@ -1639,6 +1639,7 @@ LIST_IMAGES_A_B
 	}
 	img_info[VKPT_IMG_ASVGF_TAA_A].sampler = qvk.tex_sampler;
 	img_info[VKPT_IMG_ASVGF_TAA_B].sampler = qvk.tex_sampler;
+	img_info[VKPT_IMG_TAA_OUTPUT].sampler = qvk.tex_sampler;
 
 	VkWriteDescriptorSet output_img_write[NUM_IMAGES * 2];
 