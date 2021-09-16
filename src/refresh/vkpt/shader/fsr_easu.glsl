28ce2bb8a7b722fae3156f0afa12a7183ebd220b
Frank Richter <frank.richter@gmail.com>
2021-09-16 11:48:57 +0200

    Update copyright on new FSR-related files

--- src/refresh/vkpt/shader/fsr_easu.glsl
+++ src/refresh/vkpt/shader/fsr_easu.glsl
@@ -1,5 +1,6 @@
 /*
 Copyright (C) 2019, NVIDIA CORPORATION. All rights reserved.
+Copyright (C) 2021, Frank Richter. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
