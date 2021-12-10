skullernet/q2pro

Silence Clang warnings.

src/baseq2/m_move.c
@@ -408,7 +408,7 @@ void SV_NewChaseDir(edict_t *actor, edict_t *enemy, float dist)
     }
 
 // try other directions
-    if (((rand() & 3) & 1) ||  abs(deltay) > abs(deltax)) {
+    if (((rand() & 3) & 1) || fabs(deltay) > fabs(deltax)) {
         tdir = d[1];
         d[1] = d[2];
         d[2] = tdir;

NVIDIA/Q2RTX

src/baseq2/m_move.c
@@ -408,7 +408,7 @@ void SV_NewChaseDir(edict_t *actor, edict_t *enemy, float dist)
     }
 
 // try other directions
-    if (((rand() & 3) & 1) ||  fabsf(deltay) > fabsf(deltax)) {
+    if (((rand() & 3) & 1) || fabsf(deltay) > fabsf(deltax)) {
         tdir = d[1];
         d[1] = d[2];
         d[2] = tdir;