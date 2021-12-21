NVIDIA/Q2RTX

commit fb8f14a2e61984767126de1e25ae453611f6e24a

Fixed the dark outlines on some md2 models.

src/refresh/vkpt/shader/primary_rays.rgen
@@ -229,7 +229,11 @@ main()
 	vec2 tex_coord = triangle.tex_coords * bary;
 	vec3 geo_normal = normalize(triangle.normals * bary);
 
-	if (dot(geo_normal, direction) > 0)
+	vec3 flat_normal = normalize(cross(
+		triangle.positions[1] - triangle.positions[0],
+		triangle.positions[2] - triangle.positions[1]));
+	
+	if (dot(flat_normal, direction) > 0)
 		geo_normal = -geo_normal;
 	
 	/* compute view-space derivatives of depth and motion vectors */

src/refresh/vkpt/shader/reflect_refract.rgen
@@ -529,7 +529,11 @@ main()
 	vec2 tex_coord    = triangle.tex_coords * bary;
 	vec3 new_geo_normal = normalize(triangle.normals * bary);
 
-	if (dot(new_geo_normal, direction) > 0)
+	vec3 new_flat_normal = normalize(cross(
+		triangle.positions[1] - triangle.positions[0],
+		triangle.positions[2] - triangle.positions[1]));
+	
+	if (dot(new_flat_normal, direction) > 0)
 		new_geo_normal = -new_geo_normal;
 	
 	// Compute the LOD for the texture on the reflected/refracted surface.

src/refresh/vkpt/shader/indirect_lighting.rgen
@@ -330,7 +330,11 @@ indirect_lighting(
 		vec3 bounce_position = triangle.positions * bary;
 		vec3 bounce_geo_normal = normalize(triangle.normals * bary);
 			
-		if (dot(bounce_geo_normal, bounce_direction) > 0)
+		vec3 bounce_flat_normal = normalize(cross(
+			triangle.positions[1] - triangle.positions[0],
+			triangle.positions[2] - triangle.positions[1]));
+		
+		if (dot(bounce_flat_normal, bounce_direction) > 0)
 			bounce_geo_normal = -bounce_geo_normal;
 		
 		vec3 bounce_normal  = bounce_geo_normal;

commit a0298a81c0e9b18dfc2521390f5f16897e772165

Fixed the lighting on the first person weapon when it's left-handed. It was broken since fb8f14a2e61984767126de1e25ae453611f6e24a.

src/refresh/vkpt/main.c
@@ -2846,6 +2846,8 @@ R_RenderFrame_RTX(refdef_t *fd)
 	else
 		Vector4Set(ubo->fs_blend_color, 0.f, 0.f, 0.f, 0.f);
 
+	ubo->weapon_left_handed = upload_info.weapon_left_handed;
+
 	vkpt_physical_sky_update_ubo(ubo, &sun_light, render_world);
 	vkpt_bloom_update(ubo, frame_time, ubo->medium != MEDIUM_NONE, qvk.frame_menu_mode);
 

src/refresh/vkpt/shader/global_ubo.h
@@ -224,6 +224,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 	GLOBAL_UBO_VAR_LIST_DO(mat4,            security_camera_data[MAX_CAMERAS]) \
 	GLOBAL_UBO_VAR_LIST_DO(ShaderFogVolume, fog_volumes[MAX_FOG_VOLUMES]) \
 	\
+	GLOBAL_UBO_VAR_LIST_DO(int,             weapon_left_handed) \
+	\
 	UBO_CVAR_LIST // WARNING: Do not put any other members into global_ubo after this: the CVAR list is not vec4-aligned
 
 

src/refresh/vkpt/shader/primary_rays.rgen
@@ -230,8 +230,10 @@ main()
 	vec3 flat_normal = normalize(cross(
 		triangle.positions[1] - triangle.positions[0],
 		triangle.positions[2] - triangle.positions[1]));
+
+	float flat_normal_sign = (global_ubo.weapon_left_handed != 0 && (triangle.material_id & MATERIAL_FLAG_WEAPON) != 0) ? -1.0 : 1.0;
 	
-	if (dot(flat_normal, direction) > 0)
+	if (dot(flat_normal, direction) * flat_normal_sign > 0)
 		geo_normal = -geo_normal;
 	
 	/* compute view-space derivatives of depth and motion vectors */

src/refresh/vkpt/shader/reflect_refract.rgen
@@ -541,7 +541,9 @@ main()
 		triangle.positions[1] - triangle.positions[0],
 		triangle.positions[2] - triangle.positions[1]));
 	
-	if (dot(new_flat_normal, direction) > 0)
+	float flat_normal_sign = (global_ubo.weapon_left_handed != 0 && (triangle.material_id & MATERIAL_FLAG_WEAPON) != 0) ? -1.0 : 1.0;
+	
+	if (dot(new_flat_normal, direction) * flat_normal_sign > 0)
 		new_geo_normal = -new_geo_normal;
 	
 	// Compute the LOD for the texture on the reflected/refracted surface.