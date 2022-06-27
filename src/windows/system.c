625bea535230ec6df0c7d8096adc87e48e6f41a8
Andrey Nazarov <skuller@skuller.net>
2022-06-27 20:24:16 +0300

    Exit on key press after fatal error.

--- src/windows/system.c
+++ src/windows/system.c
@@ -211,6 +211,19 @@ static void clear_console_window(void)
     show_console_input();
 }
 
+static void wait_console_key(void)
+{
+    INPUT_RECORD rec;
+    DWORD res;
+
+    while (1) {
+        if (!ReadConsoleInput(hinput, &rec, 1, &res))
+            break;
+        if (rec.EventType == KEY_EVENT && rec.Event.KeyEvent.bKeyDown)
+            break;
+    }
+}
+
 /*
 ================
 Sys_ConsoleInput
@@ -974,7 +987,9 @@ void Sys_Error(const char *error, ...)
 #if USE_SYSCON
     if (gotConsole) {
         hide_console_input();
-        Sleep(INFINITE);
+        Sys_Printf("Press any key to exit.\n");
+        wait_console_key();
+        exit(1);
     }
 #endif
 
