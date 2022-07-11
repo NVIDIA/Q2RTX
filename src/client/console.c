50ad22da8c8ec508628f067ac912d2310fde2917
Andrey Nazarov <skuller@skuller.net>
2022-07-11 18:04:14 +0300

    Replace non-printable characters with question marks when pasting.

--- src/client/console.c
+++ src/client/console.c
@@ -1158,9 +1158,10 @@ static void Con_Paste(char *(*func)(void))
             IF_CharEvent(&con.prompt.inputLine, ' ');
             break;
         default:
-            if (Q_isprint(c)) {
-                IF_CharEvent(&con.prompt.inputLine, c);
+            if (!Q_isprint(c)) {
+                c = '?';
             }
+            IF_CharEvent(&con.prompt.inputLine, c);
             break;
         }
     }
