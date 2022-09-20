c4863015c9a2bba5b96ed846509d13418966c61a
Andrey Nazarov <skuller@skuller.net>
2022-09-20 03:12:46 +0300

    Fix a typo.

--- src/baseq2/p_hud.c
+++ src/baseq2/p_hud.c
@@ -53,7 +53,7 @@ void MoveClientToIntermission(edict_t *ent)
     ent->s.modelindex = 0;
     ent->s.modelindex2 = 0;
     ent->s.modelindex3 = 0;
-    ent->s.modelindex = 0;
+    ent->s.modelindex4 = 0;
     ent->s.effects = 0;
     ent->s.sound = 0;
     ent->solid = SOLID_NOT;
