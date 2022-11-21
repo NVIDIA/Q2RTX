5138c79cc80587c3ab0e7da1cc115a186f1f25fc
Andrey Nazarov <skuller@skuller.net>
2022-11-21 20:56:40 +0300

    Restore original waterlevel jump sound check.
    Probably better to keep original behavior after all (no sound when
    jumping in shallow water).

--- src/baseq2/p_client.c
+++ src/baseq2/p_client.c
@@ -1619,7 +1619,7 @@ void ClientThink(edict_t *ent, usercmd_t *ucmd)
         client->resp.cmd_angles[1] = SHORT2ANGLE(ucmd->angles[1]);
         client->resp.cmd_angles[2] = SHORT2ANGLE(ucmd->angles[2]);
 
-        if (~client->ps.pmove.pm_flags & pm.s.pm_flags & PMF_JUMP_HELD && !(pm.watertype & CONTENTS_LAVA)) {
+        if (~client->ps.pmove.pm_flags & pm.s.pm_flags & PMF_JUMP_HELD && pm.waterlevel == 0) {
             gi.sound(ent, CHAN_VOICE, gi.soundindex("*jump1.wav"), 1, ATTN_NORM, 0);
             PlayerNoise(ent, ent->s.origin, PNOISE_SELF);
         }
