69b6391aa8ae726da28e060c59ed5ef093e00ce5
Andrey Nazarov <skuller@skuller.net>
2022-12-27 14:07:52 +0300

    Allow specifying message level for ‘_echo’ command.

--- src/common/cmd.c
+++ src/common/cmd.c
@@ -1738,6 +1738,7 @@ static const cmd_option_t o_echo[] = {
     { "h", "help", "display this message" },
     { "e", "escapes", "enable interpretation of backslash escapes" },
     { "c:color", "color", "print text in this color" },
+    { "l:level", "level", "print text at this message level" },
     { "n", "no-newline", "do not output the trailing newline" },
     { NULL }
 };
@@ -1791,6 +1792,7 @@ static void Cmd_EchoEx_f(void)
 {
     char buffer[MAX_STRING_CHARS], *s;
     bool escapes = false;
+    print_type_t level = PRINT_ALL;
     color_index_t color = COLOR_NONE;
     const char *newline = "\n";
     int c;
@@ -1808,6 +1810,19 @@ static void Cmd_EchoEx_f(void)
         case 'c':
             color = Com_ParseColor(cmd_optarg);
             break;
+        case 'l':
+            switch (Q_tolower(*cmd_optarg)) {
+            case 'a': level = PRINT_ALL;       break;
+            case 't': level = PRINT_TALK;      break;
+            case 'd': level = PRINT_DEVELOPER; break;
+            case 'w': level = PRINT_WARNING;   break;
+            case 'e': level = PRINT_ERROR;     break;
+            case 'n': level = PRINT_NOTICE;    break;
+            default:
+                Com_Printf("Bad print level: %s\n", cmd_optarg);
+                return;
+            }
+            break;
         case 'n':
             newline = "";
             break;
@@ -1822,7 +1837,7 @@ static void Cmd_EchoEx_f(void)
     }
 
     Com_SetColor(color);
-    Com_Printf("%s%s", s, newline);
+    Com_LPrintf(level, "%s%s", s, newline);
     Com_SetColor(COLOR_NONE);
 }
 
