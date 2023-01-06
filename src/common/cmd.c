356fbcddf6c5e36277a4cbb34d94d210d3ff06d6
Andrey Nazarov <skuller@skuller.net>
2023-01-06 14:08:49 +0300

    Skip leading whitespace in Cmd_TokenizeString().

--- src/common/cmd.c
+++ src/common/cmd.c
@@ -1325,11 +1325,11 @@ $Cvars will be expanded unless they are in a quoted token
 */
 void Cmd_TokenizeString(const char *text, bool macroExpand)
 {
-    int     i, len;
+    size_t  len;
     char    *data, *dest;
 
 // clear the args from the last string
-    for (i = 0; i < cmd_argc; i++) {
+    for (int i = 0; i < cmd_argc; i++) {
         cmd_argv[i] = NULL;
         cmd_offsets[i] = 0;
     }
@@ -1352,6 +1352,11 @@ void Cmd_TokenizeString(const char *text, bool macroExpand)
         }
     }
 
+// skip any leading whitespace
+    while (*text && *text <= ' ') {
+        text++;
+    }
+
 // strip off any trailing whitespace
     len = strlen(text);
     while (len > 0 && text[len - 1] <= ' ') {
@@ -1359,6 +1364,9 @@ void Cmd_TokenizeString(const char *text, bool macroExpand)
     }
     if (len >= MAX_STRING_CHARS) {
         Com_Printf("Line exceeded %i chars, discarded.\n", MAX_STRING_CHARS);
+        return;
+    }
+    if (!len) {
         return;
     }
 
