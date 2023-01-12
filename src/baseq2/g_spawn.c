87ca687fcb28729bec0793454878a456c8d3f2f5
Andrey Nazarov <skuller@skuller.net>
2023-01-12 19:26:25 +0300

    Compress status bar whitespace.

--- src/baseq2/g_spawn.c
+++ src/baseq2/g_spawn.c
@@ -707,50 +707,50 @@ static const char single_statusbar[] =
 
 // ammo
 "if 2 "
-"   xv  100 "
-"   anum "
-"   xv  150 "
-"   pic 2 "
+  "xv 100 "
+  "anum "
+  "xv 150 "
+  "pic 2 "
 "endif "
 
 // armor
 "if 4 "
-"   xv  200 "
-"   rnum "
-"   xv  250 "
-"   pic 4 "
+  "xv 200 "
+  "rnum "
+  "xv 250 "
+  "pic 4 "
 "endif "
 
 // selected item
 "if 6 "
-"   xv  296 "
-"   pic 6 "
+  "xv 296 "
+  "pic 6 "
 "endif "
 
 "yb -50 "
 
 // picked up item
 "if 7 "
-"   xv  0 "
-"   pic 7 "
-"   xv  26 "
-"   yb  -42 "
-"   stat_string 8 "
-"   yb  -50 "
+  "xv 0 "
+  "pic 7 "
+  "xv 26 "
+  "yb -42 "
+  "stat_string 8 "
+  "yb -50 "
 "endif "
 
 // timer
 "if 9 "
-"   xv  262 "
-"   num 2   10 "
-"   xv  296 "
-"   pic 9 "
+  "xv 262 "
+  "num 2 10 "
+  "xv 296 "
+  "pic 9 "
 "endif "
 
-//  help / weapon icon
+// help / weapon icon
 "if 11 "
-"   xv  148 "
-"   pic 11 "
+  "xv 148 "
+  "pic 11 "
 "endif "
 ;
 
@@ -765,71 +765,71 @@ static const char dm_statusbar[] =
 
 // ammo
 "if 2 "
-"   xv  100 "
-"   anum "
-"   xv  150 "
-"   pic 2 "
+  "xv 100 "
+  "anum "
+  "xv 150 "
+  "pic 2 "
 "endif "
 
 // armor
 "if 4 "
-"   xv  200 "
-"   rnum "
-"   xv  250 "
-"   pic 4 "
+  "xv 200 "
+  "rnum "
+  "xv 250 "
+  "pic 4 "
 "endif "
 
 // selected item
 "if 6 "
-"   xv  296 "
-"   pic 6 "
+  "xv 296 "
+  "pic 6 "
 "endif "
 
 "yb -50 "
 
 // picked up item
 "if 7 "
-"   xv  0 "
-"   pic 7 "
-"   xv  26 "
-"   yb  -42 "
-"   stat_string 8 "
-"   yb  -50 "
+  "xv 0 "
+  "pic 7 "
+  "xv 26 "
+  "yb -42 "
+  "stat_string 8 "
+  "yb -50 "
 "endif "
 
 // timer
 "if 9 "
-"   xv  246 "
-"   num 2   10 "
-"   xv  296 "
-"   pic 9 "
+  "xv 246 "
+  "num 2 10 "
+  "xv 296 "
+  "pic 9 "
 "endif "
 
-//  help / weapon icon
+// help / weapon icon
 "if 11 "
-"   xv  148 "
-"   pic 11 "
+  "xv 148 "
+  "pic 11 "
 "endif "
 
-//  frags
+// frags
 "xr -50 "
 "yt 2 "
 "num 3 14 "
 
 // spectator
 "if 17 "
-"xv 0 "
-"yb -58 "
-"string2 \"SPECTATOR MODE\" "
+  "xv 0 "
+  "yb -58 "
+  "string2 \"SPECTATOR MODE\" "
 "endif "
 
 // chase camera
 "if 16 "
-"xv 0 "
-"yb -68 "
-"string \"Chasing\" "
-"xv 64 "
-"stat_string 16 "
+  "xv 0 "
+  "yb -68 "
+  "string \"Chasing\" "
+  "xv 64 "
+  "stat_string 16 "
 "endif "
 ;
 
