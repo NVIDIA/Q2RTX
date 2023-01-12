fe3b28154316beaaa88681be7a1a537c2f3541ba
Andrey Nazarov <skuller@skuller.net>
2023-01-12 19:24:34 +0300

    Clear more entity state on respawn/disconnect.

--- src/baseq2/p_client.c
+++ src/baseq2/p_client.c
@@ -1176,7 +1176,9 @@ void PutClientInServer(edict_t *ent)
     client->ps.gunindex = gi.modelindex(client->pers.weapon->view_model);
 
     // clear entity state values
+    ent->s.sound = 0;
     ent->s.effects = 0;
+    ent->s.renderfx = 0;
     ent->s.modelindex = 255;        // will use the skin specified model
     ent->s.modelindex2 = 255;       // custom gun model
     // sknum is player num and weapon number
@@ -1498,9 +1500,12 @@ void ClientDisconnect(edict_t *ent)
 
     gi.unlinkentity(ent);
     ent->s.modelindex = 0;
+    ent->s.modelindex2 = 0;
     ent->s.sound = 0;
     ent->s.event = 0;
     ent->s.effects = 0;
+    ent->s.renderfx = 0;
+    ent->s.solid = 0;
     ent->solid = SOLID_NOT;
     ent->inuse = false;
     ent->classname = "disconnected";
