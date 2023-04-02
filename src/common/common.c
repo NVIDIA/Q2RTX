fe8e71bddf6342ebfbb10732d2b7076506df5414
Andrey Nazarov <skuller@skuller.net>
2023-04-02 22:53:00 +0300

    Fix warning when building without USE_DEBUG.

--- src/common/common.c
+++ src/common/common.c
@@ -51,7 +51,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "system/system.h"
 #include "system/hunk.h"
 
+#if USE_DEBUG
 #include "features.h"
+#endif
 
 #include <setjmp.h>
 
