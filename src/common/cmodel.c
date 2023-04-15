12ae28e8747deb567c5b20feeec09359a27d075c
Andrey Nazarov <skuller@skuller.net>
2023-04-15 19:16:17 +0300

    Drop second part of assert.
    It was not originally there and is kinda pointless.

--- src/common/cmodel.c
+++ src/common/cmodel.c
@@ -1114,7 +1114,7 @@ byte *CM_FatPVS(cm_t *cm, byte *mask, const vec3_t org, int vis)
     }
 
     count = CM_BoxLeafs(cm, mins, maxs, leafs, q_countof(leafs), NULL);
-    Q_assert(count > 0 && count <= q_countof(leafs));
+    Q_assert(count > 0);
 
     // convert leafs to clusters
     for (i = 0; i < count; i++) {
