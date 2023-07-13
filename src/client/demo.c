264f28a7e282586f18f8f6811aed1158ff8a69a3
Andrey Nazarov <skuller@skuller.net>
2023-07-13 19:29:43 +0300

    Allow getting old demos info.

--- src/client/demo.c
+++ src/client/demo.c
@@ -1151,7 +1151,8 @@ demoInfo_t *CL_GetDemoInfo(const char *path, demoInfo_t *info)
         if (MSG_ReadByte() != svc_serverdata) {
             goto fail;
         }
-        if (MSG_ReadLong() != PROTOCOL_VERSION_DEFAULT) {
+        c = MSG_ReadLong();
+        if (c < PROTOCOL_VERSION_OLD || c > PROTOCOL_VERSION_DEFAULT) {
             goto fail;
         }
         MSG_ReadLong();
@@ -1213,7 +1214,6 @@ demoInfo_t *CL_GetDemoInfo(const char *path, demoInfo_t *info)
 fail:
     FS_CloseFile(f);
     return NULL;
-
 }
 
 // =========================================================================
