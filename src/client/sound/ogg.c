skullernet/q2pro

Don't allow OGG file with zero samples.

src/client/sound/ogg.c
@@ -283,7 +283,7 @@ bool OGG_Load(sizebuf_t *sz)
     }
 
     ogg_int64_t samples = ov_pcm_total(&vf, 0);
-    if (samples < 0 || samples > MAX_LOADFILE >> vi->channels) {
+    if (samples < 1 || samples > MAX_LOADFILE >> vi->channels) {
         Com_DPrintf("%s has bad number of samples\n", s_info.name);
         goto fail;
     }

NVIDIA/Q2RTX

src/client/sound/ogg.c
@@ -509,7 +509,7 @@ bool OGG_Load(sizebuf_t *sz)
 	}
 
 	unsigned int samples = stb_vorbis_stream_length_in_samples(vf);
-	if (samples > MAX_LOADFILE >> vf->channels) {
+	if (samples < 1 || samples > MAX_LOADFILE >> vf->channels) {
 		Com_DPrintf("%s has bad number of samples\n", s_info.name);
 		goto fail;
 	}