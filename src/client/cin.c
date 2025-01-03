skullernet/q2pro

Remove unneeded checks for NULL before Z_Free().
FS_FCloseFile() is safe to call with 0 handle as well.

src/client/cin.c
@@ -79,10 +79,8 @@ SCR_StopCinematic
 */
 void SCR_StopCinematic(void)
 {
-    if (cin.pic)
-        Z_Free(cin.pic);
-    if (cin.file)
-        FS_FCloseFile(cin.file);
+    Z_Free(cin.pic);
+    FS_FCloseFile(cin.file);
     memset(&cin, 0, sizeof(cin));
 }
 

NVIDIA/Q2RTX

src/client/cin.c
@@ -81,10 +81,8 @@ void SCR_StopCinematic(void)
 {
     R_DiscardRawPic();
 
-    if (cin.pic)
-        Z_Free(cin.pic);
-    if (cin.file)
-        FS_CloseFile(cin.file);
+    Z_Free(cin.pic);
+    FS_CloseFile(cin.file);
     memset(&cin, 0, sizeof(cin));
 }
 