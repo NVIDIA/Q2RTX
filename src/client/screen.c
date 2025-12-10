NVIDIA/Q2RTX

commit 8897a75b5c65884ea5e0c6b8e6dbb888a00c6a04

Initial support for protocol limit removal.

src/client/screen.c
@@ -1473,11 +1473,11 @@ static void SCR_DrawInventory(void)
     for (i = top; i < num && i < top + DISPLAY_ITEMS; i++) {
         item = index[i];
         // search for a binding
-        Q_concat(string, sizeof(string), "use ", cl.configstrings[CS_ITEMS + item]);
+        Q_concat(string, sizeof(string), "use ", cl.configstrings[cl.csr.items + item]);
         bind = Key_GetBinding(string);
 
         Q_snprintf(string, sizeof(string), "%6s %3i %s",
-                   bind, cl.inventory[item], cl.configstrings[CS_ITEMS + item]);
+                   bind, cl.inventory[item], cl.configstrings[cl.csr.items + item]);
 
         if (item != selected) {
             HUD_DrawAltString(x, y, string);
@@ -1593,11 +1593,11 @@ static void SCR_ExecuteLayoutString(const char *s)
                 Com_Error(ERR_DROP, "%s: invalid stat index", __func__);
             }
             index = cl.frame.ps.stats[value];
-            if (index < 0 || index >= MAX_IMAGES) {
+            if (index < 0 || index >= cl.csr.max_images) {
                 Com_Error(ERR_DROP, "%s: invalid pic index", __func__);
             }
-            token = cl.configstrings[CS_IMAGES + index];
-            if (token[0] && cl.image_precache[index]) {
+            token = cl.configstrings[cl.csr.images + index];
+            if (token[0]) {
                 qhandle_t pic = cl.image_precache[index];
                 // hack for action mod scope scaling
                 if (x == scr.hud_width  / 2 - 160 &&
@@ -1782,7 +1782,7 @@ static void SCR_ExecuteLayoutString(const char *s)
                 Com_Error(ERR_DROP, "%s: invalid stat index", __func__);
             }
             index = cl.frame.ps.stats[index];
-            if (index < 0 || index >= MAX_CONFIGSTRINGS) {
+            if (index < 0 || index >= cl.csr.end) {
                 Com_Error(ERR_DROP, "%s: invalid string index", __func__);
             }
             HUD_DrawString(x, y, cl.configstrings[index]);

inc/common/msg.h
@@ -30,16 +30,16 @@ typedef struct {
     int16_t     origin[3];
     int16_t     angles[3];
     int16_t     old_origin[3];
-    uint8_t     modelindex;
-    uint8_t     modelindex2;
-    uint8_t     modelindex3;
-    uint8_t     modelindex4;
+    uint16_t    modelindex;
+    uint16_t    modelindex2;
+    uint16_t    modelindex3;
+    uint16_t    modelindex4;
     uint32_t    skinnum;
     uint32_t    effects;
     uint32_t    renderfx;
     uint32_t    solid;
     uint16_t    frame;
-    uint8_t     sound;
+    uint16_t    sound;
     uint8_t     event;
 } entity_packed_t;
 
@@ -50,7 +50,7 @@ typedef struct {
     int8_t          kick_angles[3];
     int8_t          gunangles[3];
     int8_t          gunoffset[3];
-    uint8_t         gunindex;
+    uint16_t        gunindex;
     uint8_t         gunframe;
     uint8_t         blend[4];
     uint8_t         fov;
@@ -65,6 +65,7 @@ typedef enum {
     MSG_PS_IGNORE_VIEWANGLES    = BIT(3),   // ignore viewangles
     MSG_PS_IGNORE_DELTAANGLES   = BIT(4),   // ignore delta_angles
     MSG_PS_IGNORE_PREDICTION    = BIT(5),   // mutually exclusive with IGNORE_VIEWANGLES
+    MSG_PS_EXTENSIONS           = BIT(6),   // enable protocol extensions
     MSG_PS_FORCE                = BIT(7),   // send even if unchanged (MVD stream only)
     MSG_PS_REMOVE               = BIT(8),   // player is removed (MVD stream only)
 } msgPsFlags_t;
@@ -77,7 +78,8 @@ typedef enum {
     MSG_ES_UMASK        = BIT(4),   // client has 16-bit mask MSB fix
     MSG_ES_BEAMORIGIN   = BIT(5),   // client has RF_BEAM old_origin fix
     MSG_ES_SHORTANGLES  = BIT(6),   // higher precision angles encoding
-    MSG_ES_REMOVE       = BIT(7),   // entity is removed (MVD stream only)
+    MSG_ES_EXTENSIONS   = BIT(7),   // enable protocol extensions
+    MSG_ES_REMOVE       = BIT(8),   // entity is removed (MVD stream only)
 } msgEsFlags_t;
 
 extern sizebuf_t    msg_write;
@@ -111,7 +113,7 @@ void    MSG_WriteDir(const vec3_t vector);
 void    MSG_PackEntity(entity_packed_t *out, const entity_state_t *in);
 void    MSG_WriteDeltaEntity(const entity_packed_t *from, const entity_packed_t *to, msgEsFlags_t flags);
 void    MSG_PackPlayer(player_packed_t *out, const player_state_t *in);
-void    MSG_WriteDeltaPlayerstate_Default(const player_packed_t *from, const player_packed_t *to);
+void    MSG_WriteDeltaPlayerstate_Default(const player_packed_t *from, const player_packed_t *to, msgPsFlags_t flags);
 int     MSG_WriteDeltaPlayerstate_Enhanced(const player_packed_t *from, player_packed_t *to, msgPsFlags_t flags);
 void    MSG_WriteDeltaPlayerstate_Packet(const player_packed_t *from, const player_packed_t *to, int number, msgPsFlags_t flags);
 
@@ -147,10 +149,10 @@ void    MSG_ReadDeltaUsercmd_Enhanced(const usercmd_t *from, usercmd_t *to);
 int     MSG_ParseEntityBits(int *bits);
 void    MSG_ParseDeltaEntity(const entity_state_t *from, entity_state_t *to, int number, int bits, msgEsFlags_t flags);
 #if USE_CLIENT
-void    MSG_ParseDeltaPlayerstate_Default(const player_state_t *from, player_state_t *to, int flags);
-void    MSG_ParseDeltaPlayerstate_Enhanced(const player_state_t *from, player_state_t *to, int flags, int extraflags);
+void    MSG_ParseDeltaPlayerstate_Default(const player_state_t *from, player_state_t *to, int flags, msgPsFlags_t psflags);
+void    MSG_ParseDeltaPlayerstate_Enhanced(const player_state_t *from, player_state_t *to, int flags, int extraflags, msgPsFlags_t psflags);
 #endif
-void    MSG_ParseDeltaPlayerstate_Packet(const player_state_t *from, player_state_t *to, int flags);
+void    MSG_ParseDeltaPlayerstate_Packet(const player_state_t *from, player_state_t *to, int flags, msgPsFlags_t psflags);
 
 #if USE_DEBUG
 #if USE_CLIENT

inc/common/protocol.h
@@ -22,13 +22,14 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 // protocol.h -- communications protocols
 //
 
-#define MAX_MSGLEN  0x8000  // max length of a message, 32k
+#define MAX_MSGLEN  0x10000     // max length of a message, 64k
 
 #define PROTOCOL_VERSION_OLD        26
 #define PROTOCOL_VERSION_DEFAULT    34
 #define PROTOCOL_VERSION_R1Q2       35
 #define PROTOCOL_VERSION_Q2PRO      36
 #define PROTOCOL_VERSION_MVD        37 // not used for UDP connections
+#define PROTOCOL_VERSION_EXTENDED   3434
 
 #define PROTOCOL_VERSION_R1Q2_MINIMUM           1903    // b6377
 #define PROTOCOL_VERSION_R1Q2_UCMD              1904    // b7387
@@ -44,10 +45,13 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #define PROTOCOL_VERSION_Q2PRO_ZLIB_DOWNLOADS   1021    // r1358
 #define PROTOCOL_VERSION_Q2PRO_CLIENTNUM_SHORT  1022    // r2161
 #define PROTOCOL_VERSION_Q2PRO_CINEMATICS       1023    // r2263
-#define PROTOCOL_VERSION_Q2PRO_CURRENT          1023    // r2263
+#define PROTOCOL_VERSION_Q2PRO_EXTENDED_LIMITS  1024    // r
+#define PROTOCOL_VERSION_Q2PRO_CURRENT          1024    // r
 
 #define PROTOCOL_VERSION_MVD_MINIMUM            2009    // r168
-#define PROTOCOL_VERSION_MVD_CURRENT            2010    // r177
+#define PROTOCOL_VERSION_MVD_DEFAULT            2010    // r177
+#define PROTOCOL_VERSION_MVD_EXTENDED_LIMITS    2011    // r
+#define PROTOCOL_VERSION_MVD_CURRENT            2011    // r
 
 #define R1Q2_SUPPORTED(x) \
     ((x) >= PROTOCOL_VERSION_R1Q2_MINIMUM && \
@@ -61,8 +65,13 @@ with this program; if not, write to the Free Software Foundation, Inc.,
     ((x) >= PROTOCOL_VERSION_MVD_MINIMUM && \
      (x) <= PROTOCOL_VERSION_MVD_CURRENT)
 
-#define VALIDATE_CLIENTNUM(x) \
-    ((x) >= -1 && (x) < MAX_EDICTS - 1)
+#define VALIDATE_CLIENTNUM(csr, x) \
+    ((x) >= -1 && (x) < (csr)->max_edicts - 1)
+
+#define Q2PRO_PF_STRAFEJUMP_HACK    BIT(0)
+#define Q2PRO_PF_QW_MODE            BIT(1)
+#define Q2PRO_PF_WATERJUMP_HACK     BIT(2)
+#define Q2PRO_PF_EXTENSIONS         BIT(3)
 
 //=========================================
 
@@ -171,7 +180,7 @@ typedef enum {
 typedef enum {
     MVF_NOMSGS      = BIT(0),
     MVF_SINGLEPOV   = BIT(1),
-    MVF_RESERVED    = BIT(2)
+    MVF_EXTLIMITS   = BIT(2)
 } mvd_flags_t;
 
 //==============================================
@@ -282,8 +291,9 @@ typedef enum {
 #define SND_VOLUME          BIT(0)  // a byte
 #define SND_ATTENUATION     BIT(1)  // a byte
 #define SND_POS             BIT(2)  // three coordinates
-#define SND_ENT             BIT(3)  // a short 0-2: channel, 3-12: entity
+#define SND_ENT             BIT(3)  // a short 0-2: channel, 3-15: entity
 #define SND_OFFSET          BIT(4)  // a byte, msec offset from frame start
+#define SND_INDEX16         BIT(5)  // index is 16-bit
 
 #define DEFAULT_SOUND_PACKET_VOLUME         1.0f
 #define DEFAULT_SOUND_PACKET_ATTENUATION    1.0f
@@ -327,6 +337,7 @@ typedef enum {
 #define U_SKIN16        BIT(25)
 #define U_SOUND         BIT(26)
 #define U_SOLID         BIT(27)
+#define U_MODEL16       BIT(28)
 
 #define U_SKIN32        (U_SKIN8 | U_SKIN16)        // used for laser colors
 #define U_EFFECTS32     (U_EFFECTS8 | U_EFFECTS16)

inc/common/utils.h
@@ -67,3 +67,8 @@ color_index_t Com_ParseColor(const char *s);
 #if USE_DEBUG
 char *Com_MakePrintable(const char *s);
 #endif
+
+// Some mods actually exploit CS_STATUSBAR to take space up to CS_AIRACCEL
+#define CS_SIZE(csr, cs) \
+    ((cs) >= CS_STATUSBAR && (cs) < (csr)->airaccel ? \
+      MAX_QPATH * ((csr)->airaccel - (cs)) : MAX_QPATH)

inc/refresh/images.h
@@ -71,8 +71,8 @@ typedef struct image_s {
     int             baselen; // without extension
     imagetype_t     type;
     imageflags_t    flags;
-    int             width, height; // source image
-    int             upload_width, upload_height; // after power of two and picmip
+    int16_t         width, height; // source image
+    int16_t         upload_width, upload_height; // after power of two and picmip
     int             registration_sequence; // 0 = free
 	char            filepath[MAX_QPATH]; // actual path loaded, with correct format extension
 	int             is_srgb;
@@ -119,7 +119,7 @@ void IMG_GetPalette(void);
 
 image_t *IMG_ForHandle(qhandle_t h);
 
-int IMG_GetDimensions(const char* name, int* width, int* height);
+int IMG_GetDimensions(const char* name, int16_t* width, int16_t* height);
 
 void IMG_ResampleTexture(const byte *in, int inwidth, int inheight,
                          byte *out, int outwidth, int outheight);

inc/refresh/refresh.h
@@ -23,7 +23,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "common/error.h"
 
 #define MAX_DLIGHTS     32
-#define MAX_ENTITIES    1024     // == MAX_PACKET_ENTITIES * 2
+#define MAX_ENTITIES    2048
 #define MAX_PARTICLES   16384
 #define MAX_LIGHTSTYLES 256
 

inc/shared/game.h
@@ -55,6 +55,7 @@ typedef enum {
 #define GMF_EXTRA_USERINFO          BIT(12)     // game wants extra userinfo after normal userinfo
 #define GMF_IPV6_ADDRESS_AWARE      BIT(13)     // game supports IPv6 addresses
 #define GMF_ALLOW_INDEX_OVERFLOW    BIT(14)     // game wants PF_FindIndex() to return 0 on overflow
+#define GMF_PROTOCOL_EXTENSIONS     BIT(15)     // game supports protocol extensions
 
 //===============================================================
 

inc/shared/shared.h
@@ -73,13 +73,25 @@ typedef int qhandle_t;
 // per-level limits
 //
 #define MAX_CLIENTS         256     // absolute limit
-#define MAX_EDICTS          1024    // must change protocol to increase more
+#define MAX_EDICTS_OLD      1024    // must change protocol to increase more
+#define MAX_MODELS_OLD      256     // these are sent over the net as bytes
+#define MAX_SOUNDS_OLD      256     // so they cannot be blindly increased
+#define MAX_IMAGES_OLD      256
 #define MAX_LIGHTSTYLES     256
-#define MAX_MODELS          256     // these are sent over the net as bytes
-#define MAX_SOUNDS          256     // so they cannot be blindly increased
-#define MAX_IMAGES          256
 #define MAX_ITEMS           256
 #define MAX_GENERAL         (MAX_CLIENTS * 2) // general config strings
+
+#if USE_PROTOCOL_EXTENSIONS
+#define MAX_EDICTS          8192    // sent as ENTITYNUM_BITS, can't be increased
+#define MAX_MODELS          8192    // half is reserved for inline BSP models
+#define MAX_SOUNDS          2048
+#define MAX_IMAGES          2048
+#else
+#define MAX_EDICTS          MAX_EDICTS_OLD
+#define MAX_MODELS          MAX_MODELS_OLD
+#define MAX_SOUNDS          MAX_SOUNDS_OLD
+#define MAX_IMAGES          MAX_IMAGES_OLD
+#endif
 
 #define MAX_CLIENT_NAME     16
 
@@ -1162,23 +1174,73 @@ enum {
 #define CS_SKYROTATE        4
 #define CS_STATUSBAR        5       // display program string
 
-#define CS_AIRACCEL         29      // air acceleration control
-#define CS_MAXCLIENTS       30
-#define CS_MAPCHECKSUM      31      // for catching cheater maps
+#define CS_AIRACCEL_OLD         29      // air acceleration control
+#define CS_MAXCLIENTS_OLD       30
+#define CS_MAPCHECKSUM_OLD      31      // for catching cheater maps
+#define CS_MODELS_OLD           32
+#define CS_SOUNDS_OLD           (CS_MODELS_OLD + MAX_MODELS_OLD)
+#define CS_IMAGES_OLD           (CS_SOUNDS_OLD + MAX_SOUNDS_OLD)
+#define CS_LIGHTS_OLD           (CS_IMAGES_OLD + MAX_IMAGES_OLD)
+#define CS_ITEMS_OLD            (CS_LIGHTS_OLD + MAX_LIGHTSTYLES)
+#define CS_PLAYERSKINS_OLD      (CS_ITEMS_OLD + MAX_ITEMS)
+#define CS_GENERAL_OLD          (CS_PLAYERSKINS_OLD + MAX_CLIENTS)
+#define MAX_CONFIGSTRINGS_OLD   (CS_GENERAL_OLD + MAX_GENERAL)
 
-#define CS_MODELS           32
-#define CS_SOUNDS           (CS_MODELS+MAX_MODELS)
-#define CS_IMAGES           (CS_SOUNDS+MAX_SOUNDS)
-#define CS_LIGHTS           (CS_IMAGES+MAX_IMAGES)
-#define CS_ITEMS            (CS_LIGHTS+MAX_LIGHTSTYLES)
-#define CS_PLAYERSKINS      (CS_ITEMS+MAX_ITEMS)
-#define CS_GENERAL          (CS_PLAYERSKINS+MAX_CLIENTS)
-#define MAX_CONFIGSTRINGS   (CS_GENERAL+MAX_GENERAL)
+#if USE_PROTOCOL_EXTENSIONS
+#define CS_AIRACCEL         59
+#define CS_MAXCLIENTS       60
+#define CS_MAPCHECKSUM      61
+#define CS_MODELS           62
+#define CS_SOUNDS           (CS_MODELS + MAX_MODELS)
+#define CS_IMAGES           (CS_SOUNDS + MAX_SOUNDS)
+#define CS_LIGHTS           (CS_IMAGES + MAX_IMAGES)
+#define CS_ITEMS            (CS_LIGHTS + MAX_LIGHTSTYLES)
+#define CS_PLAYERSKINS      (CS_ITEMS + MAX_ITEMS)
+#define CS_GENERAL          (CS_PLAYERSKINS + MAX_CLIENTS)
+#define MAX_CONFIGSTRINGS   (CS_GENERAL + MAX_GENERAL)
+#else
+#define CS_AIRACCEL         CS_AIRACCEL_OLD
+#define CS_MAXCLIENTS       CS_MAXCLIENTS_OLD
+#define CS_MAPCHECKSUM      CS_MAPCHECKSUM_OLD
+#define CS_MODELS           CS_MODELS_OLD
+#define CS_SOUNDS           CS_SOUNDS_OLD
+#define CS_IMAGES           CS_IMAGES_OLD
+#define CS_LIGHTS           CS_LIGHTS_OLD
+#define CS_ITEMS            CS_ITEMS_OLD
+#define CS_PLAYERSKINS      CS_PLAYERSKINS_OLD
+#define CS_GENERAL          CS_GENERAL_OLD
+#define MAX_CONFIGSTRINGS   MAX_CONFIGSTRINGS_OLD
+#endif
 
-// Some mods actually exploit CS_STATUSBAR to take space up to CS_AIRACCEL
-#define CS_SIZE(cs) \
-    ((cs) >= CS_STATUSBAR && (cs) < CS_AIRACCEL ? \
-      MAX_QPATH * (CS_AIRACCEL - (cs)) : MAX_QPATH)
+#if USE_PROTOCOL_EXTENSIONS
+
+typedef struct {
+    bool        extended;
+
+    uint16_t    max_edicts;
+    uint16_t    max_models;
+    uint16_t    max_sounds;
+    uint16_t    max_images;
+
+    uint16_t    airaccel;
+    uint16_t    maxclients;
+    uint16_t    mapchecksum;
+
+    uint16_t    models;
+    uint16_t    sounds;
+    uint16_t    images;
+    uint16_t    lights;
+    uint16_t    items;
+    uint16_t    playerskins;
+    uint16_t    general;
+
+    uint16_t    end;
+} cs_remap_t;
+
+extern const cs_remap_t     cs_remap_old;
+extern const cs_remap_t     cs_remap_new;
+
+#endif
 
 //==============================================
 
@@ -1250,3 +1312,10 @@ typedef struct {
 
     short       stats[MAX_STATS];       // fast status bar updates
 } player_state_t;
+
+//==============================================
+
+#if USE_PROTOCOL_EXTENSIONS
+#define GUNINDEX_BITS       13  // upper 3 bits are skinnum
+#define GUNINDEX_MASK       (BIT(GUNINDEX_BITS) - 1)
+#endif

commit 3ea70a080ca07524a02963c177257aff126769d9

Fixed corrupted rendering of missing HUD icons, such as for the flare gun, after switching the renderer. It was broken since 8897a75b5c65884ea5e0c6b8e6dbb888a00c6a04.

src/client/screen.c
@@ -1663,18 +1663,20 @@ static void SCR_ExecuteLayoutString(const char *s)
             token = cl.configstrings[cl.csr.images + index];
             if (token[0]) {
                 qhandle_t pic = cl.image_precache[index];
-                // hack for action mod scope scaling
-                if (x == scr.hud_width  / 2 - 160 &&
-                    y == scr.hud_height / 2 - 120 &&
-                    Com_WildCmp("scope?x", token))
-                {
-                    int w = 320 * ch_scale->value;
-                    int h = 240 * ch_scale->value;
-                    R_DrawStretchPic((scr.hud_width  - w) / 2 + ch_x->integer,
-                                     (scr.hud_height - h) / 2 + ch_y->integer,
-                                     w, h, pic);
-                } else {
-                    R_DrawPic(x, y, pic);
+                if (pic != 0) {
+                    // hack for action mod scope scaling
+                    if (x == scr.hud_width  / 2 - 160 &&
+                        y == scr.hud_height / 2 - 120 &&
+                        Com_WildCmp("scope?x", token))
+                    {
+                        int w = 320 * ch_scale->value;
+                        int h = 240 * ch_scale->value;
+                        R_DrawStretchPic((scr.hud_width  - w) / 2 + ch_x->integer,
+                                        (scr.hud_height - h) / 2 + ch_y->integer,
+                                        w, h, pic);
+                    } else {
+                        R_DrawPic(x, y, pic);
+                    }
                 }
             }
 